74fe27bf72f1f520dc910ea4ec3360b578b3528b
Norbertlm <noobertlm@gmail.com>
2019-01-22 17:50:40 -0500

    added create, read, update functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ActiveRecord::Base
-  has_many :movies, through: :lists
   has_many :lists
+  has_many :movies, through: :lists
+  
+  def movie_list
+    self.lists
+  end
+
+  def new_movie(movie_name)
+    movie = Movie.find_or_create_by(name: movie_name)
+    movie_list.find_or_create_by(user_id: self.id, movie_id: movie.id)
+  end
+
+  def get_movie(movie_name)
+    Movie.find_by(name: movie_name)
+  end
+
+  def update_movie_in_list(movie_name, opts)
+    movie_list.find(get_movie(movie_name)).update(opts)
+  end
+
+  def delete_movie_in_list(movie_name)
+  
+  end
+
 end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -14,29 +14,29 @@ def welcome
 end
 
 def prompts(name)
+    curr_user = check_user(name)
     inputs = show_menu
     status = true
-    check_user(name)
     
     while status do
         case inputs
         when "1"
             # Show all movies
             puts "Show all movies"
-            get_movie_list
+            get_movie_list(curr_user)
         when "2"
             # Add movie to movie list
-            add_new_movie
+            add_new_movie(curr_user)
         when "3"
             # View movie info
-            view_movie_info
+            view_movie_info(curr_user)
         when "4"
             # Update movie list entry
-            update_movie_list_entry
+            update_movie_list_entry(curr_user)
         when "5"
             # Delete movie from movie list
-            delete_movie_from_movie_list
-        when "exit"
+            delete_movie_from_movie_list(curr_user)
+        when "6"
             exit_line
             status = false
         end
@@ -44,6 +44,7 @@ def prompts(name)
         if status == false
             break
         end
+        puts "__________________________________________________"
         inputs = show_menu
     end
 end
@@ -55,7 +56,7 @@ def show_menu
     puts "3 View movie info"
     puts "4 Update movie list entry"
     puts "5 Delete movie from movie list"
-    puts "exit to exit"
+    puts "6 exit to exit"
 
     input = gets.chomp
     return input
@@ -70,30 +71,85 @@ def check_user(user_name)
 end
 
 # Get movie list for user
-def get_movie_list
-
+def get_movie_list(curr_user)
+    puts curr_user.movie_list.inspect
 end
 
 # Add movie to movie list for user
 # Create new movie and add it to movie list
-def add_new_movie
-
+def add_new_movie(curr_user)
+    puts "Enter a movie title"
+    input = gets.chomp
+    puts curr_user.new_movie(input)
+    puts "Added #{input} to your list"
 end
 
 # View specific movie info
 # Takes in movie id?
-def view_movie_info
-
+def view_movie_info(curr_user)
+    puts "Enter a movie title"
+    input = gets.chomp
+    puts "Here is #{input}"
+    puts curr_user.get_movie(input)
 end
 
 # Update entry in movie list
 # Takes in movie id?
-def update_movie_list_entry
-
+def update_movie_list_entry(curr_user)
+    puts "Enter a movie title"
+    input = gets.chomp
+    update_movie_logic(curr_user, input, update_movie_menu)
 end
 
 # Delete entry in movie list
 # Takes in a movie id?
-def delete_movie_from_movie_list
+def delete_movie_from_movie_list(curr_user)
 
+end
+
+def update_movie_menu
+    puts "1 to update rating"
+    puts "2 to update progress"
+    puts "3 to update feedback"
+    puts "4 to exit"
+    
+    input = gets.chomp
+    return input
+end
+
+def update_movie_logic(curr_user, movie_name, input)
+    status = true
+    curr_input = input
+
+    while status do
+        case curr_input
+        when "1"
+            puts "enter rating"
+            rating = gets.chomp
+            curr_user.update_movie_in_list(movie_name, {rating: rating})
+        when "2"
+            puts "enter progress"
+            progress = gets.chomp
+             
+            if progress == "true"
+                curr_user.update_movie_in_list(movie_name, {watched: true})
+            else
+                curr_user.update_movie_in_list(movie_name, {watched: false})
+            end
+        when "3"
+            puts "enter feedback"
+            feedback = gets.chomp
+            curr_user.update_movie_in_list(movie_name, {feedback: feedback})
+        when "4"
+            status = false
+            puts "Exit update movie menu"
+        end
+
+        if status == false
+            break
+        end
+
+        curr_input = update_movie_menu
+            
+    end
 end
