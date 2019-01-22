24380852d22c0a6efb56ab66d1e451cef7528ff3
Elizabeth Le <elizabethlemk@yahoo.com>
2019-01-22 16:45:32 -0500

    added girls

--- config/game_methods.rb
+++ config/game_methods.rb
@@ -1,3 +1,5 @@
+
+
 def new_game
 
   puts "What is your name?"
@@ -7,7 +9,7 @@ def new_game
   current_user = User.create(name: name, preference: preference,
     fitness: rand(0..15), intellect: rand(0..15), kindness: rand(0..15),
     money: 100)
-
+  puts "Welcome, #{current_user.name}"
 end
 
 
