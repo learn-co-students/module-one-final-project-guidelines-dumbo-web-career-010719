1729430fd2421170e6fb4e85f62dbec8fa89f399
github username <github email address>
2019-01-22 14:16:34 -0500

    adds start of new_game method

--- config/game_methods.rb
+++ config/game_methods.rb
@@ -2,8 +2,12 @@ def new_game
 
   puts "What is your name?"
   name = gets.chomp
-  prompt = TTY::prompt.new
+  prompt = TTY::Prompt.new
   preference = prompt.select("What is your sexual preference?", %w(Male Female))
+  name = User.new(name: name, preference: preference,
+    fitness: rand(0..15), intellect: rand(0..15), kindness: rand(0..15),
+    money: 100)
+    
 end
 
 
