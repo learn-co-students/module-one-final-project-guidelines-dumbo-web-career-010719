81e8ac39e1c04bd1d63efac9190a79c7ccdc79fa
Elizabeth Le <elizabethlemk@yahoo.com>
2019-01-22 17:51:16 -0500

    Added boys

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@ nikki = Lover.create(name: "Nikki", gender: "Female", personality: "Fierce", int
 kira = Lover.create(name: "Kira", gender: "Female", personality: "Sweet", interest: "volunteering", fitness_req: 10, intellect_req: 20, kindness_req: 30, money_req: 50)
 princess = Lover.create(name: "Princess", gender: "Female", personality: "Shallow", interest: "money", fitness_req: 30, intellect_req: 30, kindness_req: 10, money_req: 1000)
 penelope = Lover.create(name: "Penelope", gender: "Female", personality: "Nerdy", interest: "intellect", fitness_req: 10, intellect_req: 40, kindness_req: 10, money_req: 20)
+ryan = Lover.create(name: "Ryan", gender: "Male", personality: "Quiet", interest: "intellect", fitness_req: 5, intellect_req: 30, kindness_req: 10, money_req: 20)
+john = Lover.create(name: "John", gender: "Male", personality: "Funny", interest: "money", fitness_req: 15, intellect_req: 10, kindness_req: 10, money_req: 1000)
+fabio = Lover.create(name: "Fabio", gender: "Male", personality: "Strong", interest: "fitness", fitness_req: 30, intellect_req: 5, kindness_req: 5, money_req: 50)
+oliver = Lover.create(name: "Oliver", gender: "Male", personality: "Sweet", interest: "volunteering", fitness_req: 0, intellect_req: 15, kindness_req: 25, money_req: 10)
