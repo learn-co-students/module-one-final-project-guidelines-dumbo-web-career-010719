2caf2dc1fff0c24e7cef7847e5dd82666f10053b
Norbertlm <noobertlm@gmail.com>
2019-01-23 10:38:51 -0500

    able to produce table for movie_list

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,10 @@
 require_relative '../config/environment'
 require 'active_record'
 require 'tty-prompt'
+require 'tty-table'
+require 'pry'
+
+ActiveRecord::Base.logger = nil
 
 def command_line
     prompts(welcome)
@@ -19,14 +23,14 @@ def prompts(name)
     curr_user = check_user(name)
     
     while true do
-        if main_prompt_screen(curr_user) == false
+        if main_prompt_options(curr_user) == false
             break
         end
     end
 end
 
 def exit_line
-    puts "C ya"
+    puts "We exit"
     return false
 end
 
@@ -36,7 +40,11 @@ end
 
 # Get movie list for user
 def get_movie_list(curr_user)
-    puts curr_user.movie_list.inspect
+    movie_list = curr_user.movie_list
+    movie_list.each { |movie|
+        movie.rating
+    }
+    puts display_table(parse_movie_list(movie_list))
 end
 
 # Add movie to movie list for user
@@ -73,9 +81,9 @@ def delete_movie_from_movie_list(curr_user)
 end
 
 
-def main_prompt_screen(curr_user)
+def main_prompt_options(curr_user)
     prompt = TTY::Prompt.new
-    screen = [
+    options = [
         {"Show all movies" => -> do get_movie_list(curr_user) end},
         {"Add movie to most list" => -> do add_new_movie(curr_user) end},
         {"View movie info" => -> do view_movie_info(check_user) end},
@@ -83,18 +91,18 @@ def main_prompt_screen(curr_user)
         {"Delete movie from movie list" => -> do delete_movie_list_entry(check_user) end},
         {"Exit" => -> do exit_line end}
     ]
-    prompt.select("", screen)
+    prompt.select("", options)
 end
 
 def update_movie_prompt(curr_user, movie_name)
     prompt = TTY::Prompt.new
-    screen = [
+    options = [
         {"Update rating" => -> do update_movie_rating(curr_user, movie_name) end},
         {"Update progress" => -> do update_movie_progress(curr_user, movie_name) end},
         {"Update feedback" => -> do update_movie_feedback(curr_user, movie_name) end},
         {"Exit" => -> do update_exit end}
     ]
-    prompt.select("", screen)
+    prompt.select("", options)
 end
 
 def update_movie_rating(curr_user, movie_name)
@@ -123,4 +131,23 @@ end
 
 def update_exit
     return false
+end
+
+def parse_movie_list(movie_list)
+    movie_id = movie_list.map { |item| item.movie_id}
+    movies = movie_id.map { |id| Movie.find(id)}
+    movie_names = movies.map { |movie| movie.name}
+    output = []
+    #binding.pry
+    for i in 0...movie_names.size
+        output << [movie_names[i], movie_list[i].rating, movie_list[i].feedback, movie_list[i].watched]
+    end
+
+    output
+end
+
+def display_table(data)
+    table = TTY::Table.new(['Title', 'Rating', 'Feedback', 'Watched'], data)
+    multi_renderer = TTY::Table::Renderer::ASCII.new(table, multiline: true)
+    multi_renderer.render
 end
