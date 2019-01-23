cf78af97893741964949a288b576844a98f1550a
Jesus <jesus.jg.medina@gmail.com>
2019-01-23 12:14:51 -0500

    Change to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-nikki = Lover.create(name: "Nikki", gender: "Female", personality: "Fierce", interest: "fitness", fitness_req: 30, intellect_req: 15, kindness_req: 15, money_req: 100, first_meeting: "Oh, hi! You must be new here. My name is Nikki. I'm in accounting! Nice to meet you.")
-kira = Lover.create(name: "Kira", gender: "Female", personality: "Sweet", interest: "volunteering", fitness_req: 10, intellect_req: 20, kindness_req: 30, money_req: 50, first_meeting: "Kira: 'Hello!! I'm Kira!! Sorry it's a mess in here...")
-princess = Lover.create(name: "Princess", gender: "Female", personality: "Shallow", interest: "money", fitness_req: 30, intellect_req: 30, kindness_req: 10, money_req: 1000,first_meeting: "Excuse me. I'm using the squat rack. Wait your turn.")
-penelope = Lover.create(name: "Penelope", gender: "Female", personality: "Nerdy", interest: "intellect", fitness_req: 10, intellect_req: 40, kindness_req: 10, money_req: 20,first_meeting: "Welcome to the library. Do you need to sign up for a new card?")
-ryan = Lover.create(name: "Ryan", gender: "Male", personality: "Quiet", interest: "intellect", fitness_req: 5, intellect_req: 30, kindness_req: 10, money_req: 20)
-john = Lover.create(name: "John", gender: "Male", personality: "Funny", interest: "money", fitness_req: 15, intellect_req: 10, kindness_req: 10, money_req: 1000)
-fabio = Lover.create(name: "Fabio", gender: "Male", personality: "Strong", interest: "fitness", fitness_req: 30, intellect_req: 5, kindness_req: 5, money_req: 50)
-oliver = Lover.create(name: "Oliver", gender: "Male", personality: "Sweet", interest: "volunteering", fitness_req: 0, intellect_req: 15, kindness_req: 25, money_req: 10)
-=======
 nikki = Lover.create(name: "Nikki", gender: "Female", personality: "Fierce", interest: "fitness", fitness_req: 30, intellect_req: 15, kindness_req: 15, money_req: 100,
   fact_place: "I love leg day",
   fact_color: "My favorite color is blue",
@@ -106,4 +96,3 @@ oliver = Lover.create(name: "Oliver", gender: "Male", personality: "Sweet", inte
   aff_kindness_mod: 1,
   aff_money_mod: 10,
   aff_pts_req: 500)
->>>>>>> master
