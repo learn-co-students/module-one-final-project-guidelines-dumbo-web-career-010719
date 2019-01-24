a044bced1c1af4a57aadb54e3da1209954bca948
Elizabeth Le <elizabethlemk@yahoo.com>
2019-01-24 14:51:27 -0500

    removed pry

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,6 @@
 require_relative '../config/environment'
 require 'tty-prompt'
-require 'pry'
+
 
 pid = fork{ exec 'afplay', "./sounds/Slow_Dance.mp3" }
 banner
--- config/game_methods.rb
+++ config/game_methods.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 #-----------------------------------------------------------------------#
 #---------------------------- Main Menu --------------------------------#
 #-----------------------------------------------------------------------#
@@ -510,7 +510,7 @@ def all_lovers(current_user)
 end
 
 def user_meet_check(current_user, lover)
-  # binding.pry
+
   if lover.name == "Nikki"
     current_user.nikki = true
   elsif lover.name == "Kira"
